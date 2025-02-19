093884dcc6ca55a1a0b681f38177adad6c3d6c8b
Sergio Murillo <samb300593@gmail.com>
2025-02-19 16:26:08 -0700

    Se agrego la libreria uuid para el control de id's de los to do's. Tambien se agregaron los m'etodos al store de to do's

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,4 +1,4 @@
-import {Todo} from '../todos/models/todo.model'
+import { Todo } from '../todos/models/todo.model'
 
 const Filters = {
     All: 'all',
@@ -22,6 +22,63 @@ const initStore = () =>{
     console.log('Init Store')
 }
 
+const loadStore = () =>{
+    throw new Error('Not implemented');
+    
+}
+
+const getTodos = (filter = Filters.All) =>{
+    switch(filter){
+        case Filters.All:
+            return [...state.todos]; //new array with all the features
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done)
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done)
+        default: 
+            throw new Error (`Option ${filter} is not valid.`)
+    }
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = (description) =>{
+    if(!description) throw new Error('Description required');
+    state.todos.push(new Todo(description))
+
+}
+
+const toggleTodo=(todoId)=>{
+    throw new Error('Not implemented');
+}
+
+const deleteTodo=(todoId)=>{
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted=()=>{
+    throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter =() => {
+
+    throw new Error('Not implemented');
+}
+
 export default {
+    addTodo,
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
+    getTodos,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo
 }
